ae3bacf69915a7d5b8ee3f25a082b721172edbf9
Pakize Bozkurt <97640517+PakizeBozkurt@users.noreply.github.com>
2023-01-30 23:36:51 +0000

    Update script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const jokes = [
+  "Why did the programmer quit his job? He didn't get arrays.",
+  "Why do programmers always mix up Halloween and Christmas? Because Oct 31 equals Dec 25.",
+  "Why was the computer cold? Because it left its Windows open.",
+  "Why do developers always mix up their partners' birthday? Because they never remember their own.",
+  "Why do programmers prefer dark mode? Light attracts bugs.",
+];
