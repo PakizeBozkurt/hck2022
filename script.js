de41853e4fd83b951baae839a2530608d1b309b9
Pakize Bozkurt <97640517+PakizeBozkurt@users.noreply.github.com>
2023-01-30 23:37:20 +0000

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ const jokes = [
   "Why do developers always mix up their partners' birthday? Because they never remember their own.",
   "Why do programmers prefer dark mode? Light attracts bugs.",
 ];
+
+///This code defines an array of jokes, a function to get a random joke from the array, and an event listener to display the joke in a HTML element with the ID joke-display when the user clicks a button with the ID joke-button.
+
+function getJoke() {
+  const randomIndex = Math.floor(Math.random() * jokes.length);
+  return jokes[randomIndex];
+}
+
+const jokeButton = document.getElementById("joke-button");
+const jokeDisplay = document.getElementById("joke-display");
+
+jokeButton.addEventListener("click", function () {
+  jokeDisplay.innerHTML = getJoke();
+});
